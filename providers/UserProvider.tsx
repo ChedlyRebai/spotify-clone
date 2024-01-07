4645c332cc7ad875f1518b16d5c65270e20f34c2
chedly <chedly.rebai123@gmail.com>
2024-01-07 18:24:23 +0100

    add useUser & UserProvider

---
+++ providers/UserProvider.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { MyUserContextProvider } from "@/hooks/useUser"
+
+type UserProviderProps {
+    children: React.ReactNode
+}
+
+
+const UserProvider = ({children}:UserProviderProps) => {
+  return (
+    <MyUserContextProvider>
+        {children}
+    </MyUserContextProvider>
+  )
+}
+
+export default UserProvider
